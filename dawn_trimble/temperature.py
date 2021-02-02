b5a7c073e1f9b5b29ad9b9dd0a9b2330f41e7b4e
DawnMTrimble <dawntrimble77@gmail.com>
2021-02-01 21:02:38 -0500

    adding temperature.py challenge

---
+++ dawn_trimble/temperature.py
@@ -0,0 +1,34 @@
+#Saving a variable 
+#converting temp of 100 faren to celcius
+celcius_100 = (100-32)*(5/9)
+print (celcius_100)
+#this is a float because it is a decimal
+
+
+#Convert 0 faren to celcius
+celcius_0 = (0 - 32)*(5/9)
+print (celcius_0)
+
+#convert 34.2 faran to celcius
+#without a variable being assigned
+
+print((34.2 - 32)*(5/9))
+
+#Converting back celcius to fahrenheit
+
+far5 = (5*9/5)+32
+print (far5)
+
+#comparing 30.2 celc to 85.1 faren
+
+#30.2 celcius to fahren
+print ((30.2*9/5)+32)
+
+print ('30.2 celcius is greater than 85.1 Fahrenheit')
+
+
+
+
+
+
+
