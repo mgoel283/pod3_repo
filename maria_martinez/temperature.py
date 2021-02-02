45d405c4204a9dbfa66641f19c1db18116239b15
McLovinCSUN <mdmrtnz86@gmail.com>
2021-02-01 17:09:17 -0800

    Primitive data types challenge 1: converting temperatures

---
+++ maria_martinez/temperature.py
@@ -0,0 +1,19 @@
+#Convert a temperature of 100 degrees fahrenheit to celsius 
+celsius_100= (100-32)*(5/9)
+print(celsius_100)
+
+#Convert a temperature of 0 degrees fahrenheit to celsius 
+celsius_0 =(0-32)*(5/9)
+print(celsius_0)
+
+#Convert a temperature of 34.2 degrees fahrenheit to celsius 
+print((34.2-32)*(5/9))
+
+#Convert a temperature of 5 degrees celsius to fahrenheit?
+print((5*(9/5))+32)
+
+#What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+print('30.2 ℃ to fahrenheit:')
+print(((30.2*(9/5))+32))
+print('30.2 ℃ is greater than 85.1 ℉')
+
