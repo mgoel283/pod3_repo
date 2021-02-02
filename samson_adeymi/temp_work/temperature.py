bf01bcab18dd5a9c6c6f95d2dc12734ff9d7195c
samson adeyemi <mrwaywithwords@gmail.com>
2021-02-01 21:07:07 -0500

    adding

---
+++ samson_adeymi/temp_work/temperature.py
@@ -0,0 +1,8 @@
+print ("hello")
+celsius_100 = (100-32)*(5/9)
+print (celsius_100)
+celsius_0 = (0-32)*(5/9)
+print (celsius_0)
+print ((34.2-32)*(5/9))
+print ((5*9/5)+(32))
+print ("30 degrees celsius is hotter")
