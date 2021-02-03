9814010533a95e3d192b162cfce30a6fcb39e447
Manav Goel <mgoel283@gmail.com>
2021-02-02 19:40:54 -0500

    temp.py

---
+++ manav_goel/temperature.py
@@ -0,0 +1,14 @@
+# Converting 100F to Celcius
+celcius_100 = (100 - 32) * (5 / 9)
+print(celcius_100)
+
+# Converting 0F to Celcius
+celcius_0 = (0 - 32) * (5 / 9)
+print(celcius_0)
+
+# Converting 34.2F to Celcius
+print((34.2 - 32) * (5 / 9))
+
+
+print((5 * (9 / 5)) + 32)
+print((85.1 - 32) * (5 / 9))
