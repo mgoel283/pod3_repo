34ca0e7c0bf26ec6f0bc67221782b740e03b9323
McLovinCSUN <mclovin@Kaween-Maria.localdomain>
2021-02-22 18:48:53 -0800

    calculator project

--- maria_martinez/tip-calculator/calculations.py
+++ maria_martinez/tip-calculator/calculations.py
@@ -2,23 +2,28 @@
  #then set them equal to the information from the user
 print("~~~~~~~~~~Tip Calculator~~~~~~~~~~")
 price = int(input('Please, enter total price:\n'))
-percent = int(input('Please, enter tip %:\n'))
+percent = input('Please, enter tip percent amount :\n')
 numOfPpl=int(input('Please, enther the total number of people in your party:\n'))
 
+#Added this for loop to check if the user types in a percentage sign. if so then we remove the % sign
+for i in range(len(percent)):
+    if '%' in percent[i]:
+        percent = percent[:i]
+
 #calculate how much 10% is out of the price of bill
-percentFromPrice = price * (percent/100)
+percentFromPrice = price * (int(percent)/100)
 #add the ten percent to orginal bill
 totalPrice = price + percentFromPrice
 #divied the total price per person
-totalPircePerPerson = totalPrice/numOfPpl
+totalPircePerPerson = round(totalPrice,2)/numOfPpl
 #get the total tip person calculation
 totalTipPerPerson = percentFromPrice/numOfPpl
 
 #list out all the calculations for the user to see
 print("~~~~~~~~~~Bill~~~~~~~~~~~~~~~~")
 print(f'${price}')
-print(f"~~~~~~~~~~Tip per person~~~~~~~~~~~~~~~~~")
-print(f'${totalTipPerPerson}')
-print(f"~~~~~~~~~~Total per person~~~~~~~~~~")
-print(f'${totalPircePerPerson}')
+print(f"~~~~~~Tip per person~~~~~~~~~~~~")
+print(f'${round(totalTipPerPerson,2)}')
+print(f"~~~~~~~Total per person~~~~~~~~~~")
+print(f'${round(totalPircePerPerson,2)}')
 
